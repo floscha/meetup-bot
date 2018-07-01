6c4d8cff1b59616bea0837ca0621e26c7e211304
Florian Schäfer <florian.joh.schaefer@gmail.com>
2018-07-01 13:51:17 +0200

    Add parsing of Dialogflow request

--- app.py
+++ app.py
@@ -11,12 +11,22 @@ def index():
 
 @app.route('/dialogflow', methods=['POST'])
 def dialogflow_webhook():
-    payload = request.get_json()
+    json_data = request.get_json()
+    query_result = json_data['queryResult']
 
-    print(payload)
+    # String	The original text of the query.
+    query_text = query_result['queryText']
+    # Object	Consists of parameter_name:parameter_value pairs.
+    parameters = query_result['parameters']
+    # Object	The intent that matched the user's query.
+    intent = query_result['intent']
+    # Number 0-1	Matching score for the intent.
+    detection_confidence = query_result['intentDetectionConfidence']
+    # String	The language that was triggered during intent matching.
+    intent_language = query_result['languageCode']
 
     response = {'fulfillmentText': 'some text',
-                'fulfillmentMessages': [{'text': {'text': ['some text']}}],
+                'fulfillmentMessages': [{'text': {'text': [str(query_result)]}}],
                 'source': ''}
     json_response = jsonify(response)
 
