b8545e482a7df604e8d293537c30b267cd737ed7
Florian Schäfer <florian.joh.schaefer@gmail.com>
2018-07-01 13:31:32 +0200

    Add dummy '/dialogflow' route for webhook

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request, jsonify
 
 
 app = Flask(__name__)
@@ -7,3 +7,17 @@ app = Flask(__name__)
 @app.route('/', methods=['GET'])
 def index():
     return ('Hey there!')
+
+
+@app.route('/dialogflow', methods=['POST'])
+def dialogflow_webhook():
+    payload = request.get_json()
+
+    print(payload)
+
+    response = {'fulfillmentText': 'some text',
+                'fulfillmentMessages': [{'text': {'text': ['some text']}}],
+                'source': ''}
+    json_response = jsonify(response)
+
+    return json_response
