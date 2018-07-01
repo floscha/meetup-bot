4aedb4c8fb7c095dcb33665fd5b2ad78038bfc22
Florian Schäfer <florian.joh.schaefer@gmail.com>
2018-07-01 09:53:25 +0200

    Remove unnecessary run.py file

--- run.py
+++
@@ -1,8 +0,0 @@
-import meetup
-
-
-if __name__ == '__main__':
-    groups = meetup.find_groups('switzerland', 'machine learning')
-    for g in groups:
-        print(g['name'])
-        meetup.get_upcoming_meetups_for_group(g['urlname'])
